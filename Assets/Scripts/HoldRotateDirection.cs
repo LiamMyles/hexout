235c6cffaf7c9048e109ef1657d8351ffee948ad
Liam Myles <liamthemonkey92@gmail.com>
2019-12-12 22:30:24 +0000

    GH Pages

--- Assets/Scripts/HoldRotateDirection.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class HoldRotateDirection : MonoBehaviour
-{
-    //Used by the LaunchManager script's LaunchDirection() function
-    public bool clockwise;
-}
